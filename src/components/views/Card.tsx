166c34adb8ade1d1c616d479cb800225741c1201
Mohammad Aqib <Sheikhmohammadaqib@gmail.com>
2023-06-20 22:34:23 +0500

    update card

--- src/components/views/Card.tsx
+++ src/components/views/Card.tsx
@@ -13,14 +13,16 @@ function urlFor(source: any) {
 const Card: FC<{ singleProductData: oneProductType }> = ({ singleProductData }) => {
     if (singleProductData.image) {
         return (
-            <div className='max-w-sm min-w-[24rem] space-y-3 select-none hover:scale-110 duration-300'>
+            <div className='mx-auto w-[11rem] md:w-[16rem] space-y-3 duration-300'>
                 <div className='relative w-full'>
                     <div className='absolute inset-0 z-10' />
-                    <Image width={1000} height={1000} src={singleProductData?.image[0] ? urlFor(singleProductData?.image[0]).width(1000).height(1000).url() : "/logo.webp"} alt={singleProductData.image[0].alt} />
+                    {/* <Image width={1000} height={1000} src={singleProductData?.image[0] ? urlFor(singleProductData?.image[0]).width(1000).height(1000).url() : "/logo.webp"} alt={singleProductData.image[0].alt} /> */}
+                    <Image width={1000} height={1000} src={urlFor(singleProductData.image[0]).width(1000).height(1000).url()} alt={singleProductData.image[0].alt} />
                 </div>
                 <div className='space-y-1 text-gray-600 font-semibold text-lg select-none'>
                     <Link href={`/catalog/${singleProductData.slug.current}`}>
                         <h6>{singleProductData.productName}</h6>
+                        <p className='text-sm text-pink-500'>{singleProductData.productTypes[0]}</p>
                         <p>${singleProductData.price}</p>
                     </Link>
                 </div>
