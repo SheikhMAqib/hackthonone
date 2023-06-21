7f59aaea0527070eaf886ca9ec810ef7fb35a98c
Mohammad Aqib <Sheikhmohammadaqib@gmail.com>
2023-06-22 01:02:22 +0500

    solved cart state issues

--- src/components/views/Navbar/index.tsx
+++ src/components/views/Navbar/index.tsx
@@ -3,22 +3,22 @@ import { IoMdClose } from "react-icons/io";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useState } from "react";
 import { HiOutlineChevronDown } from "react-icons/hi";
-import { BsCart2 } from "react-icons/bs";
 import { BiSearch } from "react-icons/bi";
 import { NavbarArray, NavbarItemType } from "@/components/utils/NavbarArrayAndTypes";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import DropDown from "./subComponents/DropDown";
-
 import Expand from "./subComponents/Expand";
 import { useRouter } from "next/navigation";
+import ContextWrapper from "@/global/context";
+import Cartstate from "./subComponents/Cartstate";
 
 const Navbar = () => {
   const router = useRouter()
   const [isNavbarOpen, setNavbarOpen] = useState<boolean>(false);
-  const [cartItemNumber, setcartItemNumber] = useState<number>(0)
   const [searchQuery, setSearchQuery] = useState(" ")
+
 
   function handleSearchCalledFunc(e: any) {
     console.log(e.key, e.keyCode)
@@ -30,65 +30,70 @@ const Navbar = () => {
   }
 
   return (
-    <div className="sticky top-0 backdrop-blur-lg bg-opacityDownColor z-50">
-      <div className=" py-5 flex justify-between items-center gap-8 ">
-        <Link href={"/"} className="w-36 flex-shrink-0 ">
-          <Image width={500} height={500} src="/Logo.webp" alt="Logo" />
-        </Link>
-        <div className="hidden lg:flex justify-between items-center w-full">
-          <ul className="flex space-x-4  font-medium text-lg text-purple-950">
-            {NavbarArray.map((item: NavbarItemType, index: number) => (
-              <li
-                key={index} className="flex items-center relative  rounded-sm  px-3 py-1 hover:bg-gray-100 cursor-pointer group ">
-                <Link href={item.href}>{item.label}</Link>
-                {
-                  item.isDropDown ? <HiOutlineChevronDown className="mt-1 rotate-180 group-hover:rotate-0 duration-300 " size={15} /> : ""
-                }
-                {item.isDropDown && (
-                  <div className={`invisible group-hover:visible  absolute left-0 top-8 py-2 px-6 bg-gray-100 font-light min-w-[7.8rem]`}>
-                    <DropDown item={item} />
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-          {/* search bar */}
+    <ContextWrapper>
 
-          <div className="border flex items-center text-gray-700 px-3 rounded-md ">
-            <Link href={`/search/${searchQuery}`}>
-              <BiSearch />
-            </Link>
-            <input
-              type="text"
-              value={searchQuery}
-              onKeyDown={handleSearchCalledFunc}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="focus:outline-none pl-1 py-1 w-80 "
-              placeholder="Search in Our Store"
-            />
-          </div>
-          {/* icon card */}
-          <div className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center">
-            <div className=" w-4 h-4 absolute top-1 right-2 bg-red-400 text-xs rounded-full font-light flex justify-center items-center">
-              {cartItemNumber}
+      <div className="sticky top-0 backdrop-blur-lg bg-opacityDownColor z-50">
+        <div className=" py-5 flex justify-between items-center gap-8 ">
+          <Link href={"/"} className="w-36 flex-shrink-0 ">
+            <Image width={500} height={500} src="/Logo.webp" alt="Logo" />
+          </Link>
+          <div className="hidden lg:flex justify-between items-center w-full">
+            <ul className="flex space-x-4  font-medium text-lg text-purple-950">
+              {NavbarArray.map((item: NavbarItemType, index: number) => (
+                <li
+                  key={index} className="flex items-center relative  rounded-sm  px-3 py-1 hover:bg-gray-100 cursor-pointer group ">
+                  <Link href={item.href}>{item.label}</Link>
+                  {
+                    item.isDropDown ? <HiOutlineChevronDown className="mt-1 rotate-180 group-hover:rotate-0 duration-300 " size={15} /> : ""
+                  }
+                  {item.isDropDown && (
+                    <div className={`invisible group-hover:visible  absolute left-0 top-8 py-2 px-6 bg-gray-100 font-light min-w-[7.8rem]`}>
+                      <DropDown item={item} />
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+            {/* search bar */}
+
+            <div className="border flex items-center text-gray-700 px-3 rounded-md ">
+              <Link href={`/search/${searchQuery}`}>
+                <BiSearch />
+              </Link>
+              <input
+                type="text"
+                value={searchQuery}
+                onKeyDown={handleSearchCalledFunc}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="focus:outline-none pl-1 py-1 w-80 "
+                placeholder="Search in Our Store"
+              />
             </div>
-            <BsCart2 size={24} />
+            {/* icon card */}
+            {/* <div className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center">
+              <div className=" w-4 h-4 absolute top-1 right-2 bg-red-400 text-xs rounded-full font-light flex justify-center items-center">
+                {cartItemNumber}
+              </div>
+              <BsCart2 size={24} />
+            </div> */}
+            <Cartstate />
+          </div>
+
+          <div className="cursor-pointer" onClick={() => setNavbarOpen(!isNavbarOpen)}>
+            {isNavbarOpen ? (
+              <div className="flex lg:hidden ">
+                <IoMdClose size={29} />
+              </div>
+            ) : (
+              <div className="flex lg:hidden">
+                <GiHamburgerMenu size={25} />
+              </div>
+            )}
           </div>
         </div>
-        <div onClick={() => setNavbarOpen(!isNavbarOpen)}>
-          {isNavbarOpen ? (
-            <div className="flex lg:hidden ">
-              <IoMdClose size={29} />
-            </div>
-          ) : (
-            <div className="flex lg:hidden">
-              <GiHamburgerMenu size={25} />
-            </div>
-          )}
-        </div>
+        {isNavbarOpen && <MobileNavbar />}
       </div>
-      {isNavbarOpen && <MobileNavbar />}
-    </div>
+    </ContextWrapper>
   );
 };
 
--- src/components/views/ProductDetail/index.tsx
+++ src/components/views/ProductDetail/index.tsx
@@ -1,4 +1,5 @@
 "use client"
+import toast, { Toaster } from 'react-hot-toast';
 import { imagesType, oneProductType } from "@/components/utils/ProductsDataArrayAndType"
 import { FC, useContext, useState } from "react"
 import { client } from "../../../../sanity/lib/client";
@@ -6,6 +7,7 @@ import Image from "next/image";
 import imageUrlBuilder from '@sanity/image-url'
 import { BsCart2 } from "react-icons/bs";
 import { cartContext } from "@/global/context";
+import PortableText from "react-portable-text";
 
 const builder: any = imageUrlBuilder(client);
 
@@ -31,6 +33,13 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
         }
     };
 
+    const notification = (title: string) => {
+        toast(` ${quantity} ${title} added to Cart `, {
+            duration: 1000,
+            icon: '💕',
+            position: 'top-right'
+        })
+    }
     // AddTocart 
     function handleAddToCart() {
         let dataAddToInCart = {
@@ -38,76 +47,109 @@ const ProductDetail: FC<{ item: oneProductType }> = ({ item }) => {
             Quantity: quantity,
         }
         dispatch({ payload: "addToCart", data: dataAddToInCart })
-    }
-
-
+        notification(item.productName);
+    };
 
 
 
     return (
-        <div className=" flex flex-col lg:flex-row justify-center items-center">
-            {/* left */}
-            <div className="flex gap-x-4 md:gap-x-8">
+        <div>
+            <Toaster />
+            <div className=" flex flex-col lg:flex-row justify-center items-center">
                 {/* left */}
-                <div className="space-y-4">
-                    {
-                        item.image.map((subItem: imagesType, index: number) => (
-                            <div key={index} className="w-16 md:w-24" onClick={() => setImageForPreviewOfSelected(subItem._key)}>
-                                <Image width={1000} height={1000} alt={subItem.alt} src={urlFor(subItem).width(1000).height(1000).url()} />
-                            </div>
-                        ))
-                    }
-                </div>
-                {/* right  */}
-                <div className="w-[33rem] flex flex-wrap-0">
-                    {item.image.map((subItem: imagesType, index: number) => {
-                        if (subItem._key === imageForPreviewOfSelected) {
-                            return (
-                                < Image width={1000} height={1000} alt={subItem.alt} src={urlFor(subItem).width(1000).height(1000).url()} key={index} />
-                            )
-                        }
-                    })}
-                </div>
-            </div>
-            {/* right */}
-            <div className="p-6 space-y-8">
-                <div>
-                    <h1 className="text-3xl text-gray-800" > {item.productName} </h1>
-                    <p className="text-xl font-medium text-gray-600"> {item.productTypes[1]} </p>
-                </div>
-                <div className="space-y-2">
-                    <p className="text-lg font-semibold text-gray-600" > SELECT SIZE</p>
-                    <div className="flex gap-2  text-gray-600">
+                <div className="flex gap-x-4 md:gap-x-8">
+                    {/* left */}
+                    <div className="space-y-4">
                         {
-                            item.size.map((subItem: string, index: number) => (
-                                <div key={index} className=" hover:shadow-xl  font-semibold cursor-pointer rounded-full bg-gray-100 w-12 h-12 flex justify-center items-center"> {subItem} </div>
+                            item.image.map((subItem: imagesType, index: number) => (
+                                <div key={index} className="w-16 md:w-24" onClick={() => setImageForPreviewOfSelected(subItem._key)}>
+                                    <Image width={1000} height={1000} alt={subItem.alt} src={urlFor(subItem).width(1000).height(1000).url()} />
+                                </div>
                             ))
                         }
                     </div>
-                </div>
-                <div className="flex space-x-7">
-                    <p className="font-semibold text-xl text-gray-800">Quantity:</p>
-                    <div className="flex gap-2 items-center text-lg">
-                        <div
-                            onClick={decrementTheQuantity}
-                            className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full bg-gray-200">-</div>
-                        <p>{quantity}</p>
-                        <div
-                            onClick={incrementTheQuantity}
-                            className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full border border-gray-800">+</div>
+                    {/* right  */}
+                    <div className="w-[33rem] flex flex-wrap-0">
+                        {item.image.map((subItem: imagesType, index: number) => {
+                            if (subItem._key === imageForPreviewOfSelected) {
+                                return (
+                                    < Image width={1000} height={1000} alt={subItem.alt} src={urlFor(subItem).width(1000).height(1000).url()} key={index} />
+                                )
+                            }
+                        })}
                     </div>
                 </div>
-                <div className="flex gap-x-8 items-center">
-                    <button onClick={() => handleAddToCart()} className=" flex items-center  text-white bg-gray-900 border border-gray-500 px-4 py-2">
-                        <BsCart2 />
-                        &nbsp;
-                        &nbsp;
-                        Add to Cart
-                    </button>
-                    <p className="text-2xl font-semibold">${item.price}{".00"}</p>
+                {/* right */}
+                <div className="p-6 space-y-8">
+                    <div>
+                        <h1 className="text-3xl text-gray-800" > {item.productName} </h1>
+                        <p className="text-xl font-medium text-gray-600"> {item.productTypes[1]} </p>
+                    </div>
+                    <div className="space-y-2">
+                        <p className="text-lg font-semibold text-gray-600" > SELECT SIZE</p>
+                        <div className="flex gap-2  text-gray-600">
+                            {
+                                item.size.map((subItem: string, index: number) => (
+                                    <div key={index} className=" hover:shadow-xl  font-semibold cursor-pointer rounded-full bg-gray-100 w-12 h-12 flex justify-center items-center"> {subItem} </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className="flex space-x-7">
+                        <p className="font-semibold text-xl text-gray-800">Quantity:</p>
+                        <div className="flex gap-2 items-center text-lg">
+                            <div
+                                onClick={decrementTheQuantity}
+                                className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full bg-gray-200">-</div>
+                            <p>{quantity}</p>
+                            <div
+                                onClick={incrementTheQuantity}
+                                className="select-none cursor-pointer flex justify-center items-center w-9 h-9 rounded-full border border-gray-800">+</div>
+                        </div>
+                    </div>
+                    <div className="flex gap-x-8 items-center">
+                        <button onClick={() => handleAddToCart()} className=" flex items-center  text-white bg-gray-900 border border-gray-500 px-4 py-2">
+                            <BsCart2 />
+                            &nbsp;
+                            &nbsp;
+                            Add to Cart
+                        </button>
+                        <p className="text-2xl font-semibold">${item.price}{".00"}</p>
+                    </div>
                 </div>
 
             </div>
+            <div>
+                <div className="relative py-14 px-2 border-b border-gray-700">
+                    <h2 className="top-0 absolute text-6xl md:text-[9rem] font-bold text-gray-200 text-center mx-auto -z-50 ">Overview</h2>
+                    <p className="font-semibold text-xl">Product Information</p>
+                </div>
+
+                <div className="text-gray-600">
+                    <div className="flex  px-2 py-4">
+                        <div className="w-[100rem] ">
+                            <h3 className="font-semibold">PRODUCT DETAILS</h3>
+                        </div>
+                        <p>
+                            <PortableText content={item.description} />
+                        </p>
+                    </div>
+
+                    <div className="flex  px-2 py-8">
+                        <div className="w-80 ">
+                            <h3 className="font-semibold">PRODUCT CARE</h3>
+                        </div>
+                        <ul className="pl-3 list-disc font-semibold text-gray-900">
+                            <li>Hand wash using cold water.</li>
+                            <li>Do not using bleach.</li>
+                            <li>Hang it to dry.</li>
+                            <li>Iron on low temperature.</li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <div className="h-16" />
         </div>
     )
 }
