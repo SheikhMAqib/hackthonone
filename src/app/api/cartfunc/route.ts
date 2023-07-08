c9ee00aad466ebd6bb9b07f66955d54fd7db2179
Mohammad Aqib <Sheikhmohammadaqib@gmail.com>
2023-07-08 22:03:41 +0500

    updated sign Up

--- src/app/api/cartfunc/route.ts
+++ src/app/api/cartfunc/route.ts
@@ -1,16 +1,45 @@
 import { cartTableDrizzle, db } from "@/lib/drizzle";
-// import { db } from '@vercel/postgres';
-import { NextResponse } from "next/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
 
 
 export async function GET() {
-
     try {
         let allCartData = await db.select().from(cartTableDrizzle);
-        console.log(allCartData)
         return NextResponse.json({ allCartData })
     } catch (error) {
         console.log("error : ", (error as { message: string }).message)
         return NextResponse.json({ error })
     }
 };
+
+
+
+export async function POST(req: NextRequest) {
+    let request = await req.json();
+    try {
+        if (request.product_id && request.quantity && request.user_id) {
+            let responce = await db.insert(cartTableDrizzle).values(request).returning();
+            return NextResponse.json({ responce })
+        } else {
+            throw new Error("Please put product_id quantity user_id");
+
+        }
+    } catch (error) {
+        console.log("error : ", (error as { message: string }).message)
+        return NextResponse.json({ error })
+    }
+}
+
+
+export async function PUT(req: NextRequest) {
+    let request = await req.json();
+    try {
+        let responce = await db.update(cartTableDrizzle).set(request).where(eq(cartTableDrizzle.product_id, request.product_id)).returning();
+        return NextResponse.json({ responce })
+
+    } catch (error) {
+        console.log("error : ", (error as { message: string }).message)
+        return NextResponse.json({ error })
+    }
+}
