1c10b63c9a9c5d2ed50749536808f07aa1472798
Mohammad Aqib <Sheikhmohammadaqib@gmail.com>
2023-07-11 16:47:52 +0500

    working mini cart

--- src/components/views/Navbar/subComponents/Cartstate.tsx
+++ src/components/views/Navbar/subComponents/Cartstate.tsx
@@ -1,5 +1,4 @@
 import { cartContext } from "@/global/context"
-import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import { BsCart2 } from "react-icons/bs"
 
@@ -12,16 +11,15 @@ const Cartstate = () => {
         if (cartArray.length !== 0) {
             setQuantity(cartArray.length)
         }
-
-    }, []);
+    }, [cartArray]);
 
     return (
-        <Link href={"/cart"} className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center" >
+        <div className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center" >
             <div className=" w-4 h-4 absolute top-1 right-2 bg-red-400 text-xs rounded-full font-light flex justify-center items-center">
                 {quantity}
             </div>
             <BsCart2 size={24} />
-        </Link>
+        </div>
     )
 }
 
