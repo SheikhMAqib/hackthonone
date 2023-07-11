780b4db907b0994c1c3f5e53b0b912a106dac146
Mohammad Aqib <Sheikhmohammadaqib@gmail.com>
2023-07-11 16:51:10 +0500

    updated

--- src/components/views/Navbar/subComponents/Cartstate.tsx
+++ src/components/views/Navbar/subComponents/Cartstate.tsx
@@ -1,4 +1,5 @@
 import { cartContext } from "@/global/context"
+import Link from "next/link";
 import { useContext, useEffect, useState } from "react";
 import { BsCart2 } from "react-icons/bs"
 
@@ -14,12 +15,12 @@ const Cartstate = () => {
     }, [cartArray]);
 
     return (
-        <div className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center" >
+        <Link href={"/cart"} className="flex-shrink-0 relative h-11 w-11 bg-gray-300 rounded-full flex items-center justify-center" >
             <div className=" w-4 h-4 absolute top-1 right-2 bg-red-400 text-xs rounded-full font-light flex justify-center items-center">
                 {quantity}
             </div>
             <BsCart2 size={24} />
-        </div>
+        </Link>
     )
 }
 
